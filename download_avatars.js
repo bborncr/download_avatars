685e37773b4a69000fc6bfc1e10fc39d471c94dd
bborncr <bentley@crcibernetica.com>
2017-07-03 04:59:24 +0000

    fixed eslint errors

--- download_avatars.js
+++ download_avatars.js
@@ -8,20 +8,20 @@ var user = args[0];
 var repo = args[1];
 var contributors = {};
 var contributorLogin = "";
-var avatar_url = "";
+var avatarUrl = "";
 var filename = "";
 
 var url = `https://api.github.com/repos/${user}/${repo}/contributors`;
 
 var contributorsOptions = {
-    url: url,
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8',
-        'User-Agent': 'myGitHub app'
-    }
-}
+  url: url,
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    'Accept-Charset': 'utf-8',
+    'User-Agent': 'myGitHub app'
+  }
+};
 
 function getContributors(options, callback) {
   request(options, function (error, response, body) {
@@ -35,36 +35,36 @@ function getContributors(options, callback) {
   });
 }
 
+function getAvatar(login, url){
+
+  var avatarOptions = {
+    url: url,
+    method: 'GET',
+    headers: {
+      'User-Agent': 'myGitHub app'
+    }
+  };
+
+  filename = `./avatars/${contributorLogin}.png`;
+  console.log(`Saving to: ${filename}`);
+  request(avatarOptions).pipe(fs.createWriteStream(filename));
+}
+
 getContributors(contributorsOptions, function (body) {
   console.log(`Successfully retrieved data from Github`);
   contributors = JSON.parse(body);
   console.log(`Found ${contributors.length} contributors`);
   for (var contrib in contributors){
-    avatar_url = contributors[contrib].avatar_url;
+    avatarUrl = contributors[contrib].avatar_url;
     contributorLogin = contributors[contrib].login;
-    getAvatar(contributorLogin, avatar_url);
+    getAvatar(contributorLogin, avatarUrl);
   }
 });
 
 var avatarOptions = {
-    url: url,
-    method: 'GET',
-    headers: {
-        'User-Agent': 'myGitHub app'
-    }
-}
-
-function getAvatar(login, url){
-
-var avatarOptions = {
-    url: url,
-    method: 'GET',
-    headers: {
-        'User-Agent': 'myGitHub app'
-    }
-}
-
-filename = `./avatars/${contributorLogin}.png`;
-console.log(`Saving to: ${filename}`);
-request(avatarOptions).pipe(fs.createWriteStream(filename));
-}
+  url: url,
+  method: 'GET',
+  headers: {
+    'User-Agent': 'myGitHub app'
+  }
+};
